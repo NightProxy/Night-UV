14d4dd3ebf2c723eefa9287f7f1664f2acdb0fe7
Amplify Alt <not.amplify@night-x.com>
2024-05-26 22:41:17 +0000

    done

--- src/sw.js
+++
@@ -1,7 +0,0 @@
-importScripts('/@/bundle.js');
-importScripts('/@/config.js');
-importScripts(__uv$config.sw || '/@/sw-.js');
-
-const UV = new UVServiceWorker();
-
-self.addEventListener('fetch', (event) => event.respondWith(UV.fetch(event)));
